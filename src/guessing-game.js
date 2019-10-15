2107d8f559b2d571f5c85a61b60ce6bdc233bb34
Huriosity <sasha_gur1999@mail.ru>
2019-10-16 00:58:42 +0300

    feat: complite task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.minValue = 0;
+        this.maxValue = 0; 
+    }
 
     setRange(min, max) {
-
+        this.minValue = min;
+        this.maxValue = max;
     }
 
     guess() {
-
+        return Math.round((this.maxValue + this.minValue)/2);
     }
 
     lower() {
-
+        this.maxValue =  this.guess();
     }
 
     greater() {
-
+        this.minValue = this.guess();
     }
 }
 
